1c3f5c28fee416bc8abb71e83ce0924332e0f44e
amir-original <amirrahmani7017@gmail.com>
2022-06-09 18:15:21 +0430

    refactor class Text,TextProcessingTest

--- src/main/java/Text.java
+++ src/main/java/Text.java
@@ -13,37 +13,43 @@ public class Text implements Processor {
         this.limit = limit;
     }
 
+    @Override
+    public void printAnalyse() {
+        printer.printAnalyse();
+    }
+
     public void analyse(final String text) {
+       assert text != null : "text should not be null";
+
         String filteredText = ignoreSnippetsCode(text);
         List<String> textWords = getWords(filteredText);
         findCommonWords(textWords);
     }
 
-    private String ignoreSnippetsCode(String text) {
+    private String ignoreSnippetsCode(final String text) {
         return text.replaceAll("` ` `(?<language>\\w)(.*?)` ` `$", "");
     }
 
-    public List<String> getWords(String text) {
-        words = getListOf(findWords(text));
-        return words;
+    public List<String> getWords(final String text) {
+       return words = getListOf(findWords(text));
     }
 
-    private String[] findWords(String text) {
+    private String[] findWords(final String text) {
         return text.toLowerCase().replaceAll("[,.]", "").split(" ");
     }
 
-    private List<String> getListOf(String[] array) {
+    private List<String> getListOf(final String[] array) {
         return Arrays.asList(array);
     }
 
-    private void findCommonWords(List<String> words) {
+    private void findCommonWords(final List<String> words) {
         for (String word : words) {
             int n = getNumberOfWordRepeat(commonWords, word);
             commonWords.put(word, n);
         }
     }
 
-    private int getNumberOfWordRepeat(Map<String, Integer> commonWords, String word) {
+    private int getNumberOfWordRepeat(final Map<String, Integer> commonWords,final String word) {
         Integer wordCount = commonWords.get(word);
         return isNotNull(wordCount) ? wordCount + 1 : 1;
     }
@@ -51,11 +57,12 @@ public class Text implements Processor {
     private boolean isNotNull(Integer value) {
         return !isNull(value);
     }
+
     private boolean isNull(Integer value) {
         return value == null;
     }
 
-    public String getCommonWordsElement(Integer index) {
+    public String getCommonWordsElement(final Integer index) {
         return getElement(getSortedCommonWords(), index);
     }
 
@@ -71,7 +78,7 @@ public class Text implements Processor {
         return new TreeMap<>(bvc);
     }
 
-    private Map<String, Integer> getTopTenFirstItems(Map<String, Integer> sortedMap) {
+    private Map<String, Integer> getTopTenFirstItems(final Map<String, Integer> sortedMap) {
         Map<String, Integer> topTenFirstItems = new LinkedHashMap<>();
         int counter = 0;
         for (Map.Entry<String, Integer> entry : sortedMap.entrySet()) {
@@ -79,6 +86,7 @@ public class Text implements Processor {
                 topTenFirstItems.put(entry.getKey(), entry.getValue());
             counter++;
         }
+
         return topTenFirstItems;
     }
 
@@ -86,26 +94,26 @@ public class Text implements Processor {
         return counter < limit;
     }
 
-    public String getElement(Map<String, Integer> map, Integer index) {
+    public String getElement(final Map<String, Integer> map,final Integer index) {
         String element;
         try {
             element = getArrayOf(map)[index].toString();
-        } catch (Exception e) {
+        } catch (ArrayIndexOutOfBoundsException e) {
             printReportError(map, index);
             throw e;
         }
         return element;
     }
 
-    private Object[] getArrayOf(Map<String, Integer> map) {
+    private Object[] getArrayOf(final Map<String, Integer> map) {
         return map.keySet().toArray();
     }
 
-    private void printReportError(Map<String, Integer> map, Integer index) {
+    private void printReportError(final Map<String, Integer> map,final Integer index) {
         out.println(reportError(map, index));
     }
 
-    private String reportError(Map<String, Integer> map, Integer index) {
+    private String reportError(final Map<String, Integer> map,final Integer index) {
         return String.format("Array Index Out Of Bounds Exception." + " Array size: %d But En: %d", map.size(), index);
     }
 
@@ -115,11 +123,6 @@ public class Text implements Processor {
 
     public int getWordsSize() {
         return words.size();
-    }
-
-    @Override
-    public void printAnalyse() {
-        printer.printAnalyse();
     }
 
 }
--- src/test/java/TextProcessingTest.java
+++ src/test/java/TextProcessingTest.java
@@ -3,20 +3,23 @@ import org.junit.jupiter.api.Test;
 
 import static java.lang.System.out;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class TextProcessingTest {
-    private final int limit = 10;
+
+    private static final int ARRAY_SIZE_LIMIT = 10;
     private Text textProcessor;
 
     @BeforeEach
     void init() {
-        textProcessor = new Text(limit);
+        textProcessor = new Text(ARRAY_SIZE_LIMIT);
     }
 
     @Test
     void should_be_return_words_size_in_the_text() {
-        String text = "Hello, this is an example for you to practice. You should grab " +
-                "this text and make it as your test case.";
+        String text = "Hello, this is an example for you to practice." +
+                " You should grab this text and make it as your test case.";
+
         textProcessor.analyse(text);
         assertThat(textProcessor.getWordsSize()).isEqualTo(21);
         textProcessor.printAnalyse();
@@ -24,12 +27,12 @@ public class TextProcessingTest {
 
     @Test
     void should_be_return_most_common_words_used_in_the_text() {
-        String text = "Hello, this is an example for you to practice. You should grab " +
-                "this text and make it as your test case.";
+        String text = "Hello, this is an example for you to practice." +
+                " You should grab this text and make it as your test case.";
 
         textProcessor.analyse(text);
         out.println(textProcessor.getSortedCommonWords());
-        assertThat(textProcessor.getSortedCommonWords().size()).isEqualTo(10);
+        assertThat(textProcessor.getSortedCommonWords().size()).isEqualTo(ARRAY_SIZE_LIMIT);
         assertThat(textProcessor.getCommonWordsElement(0)).isEqualTo("you");
         assertThat(textProcessor.getCommonWordsElement(1)).isEqualTo("this");
         textProcessor.printAnalyse();
@@ -38,15 +41,16 @@ public class TextProcessingTest {
 
     @Test
     void should_be_ignore_a_given_piece_of_text_to_analyse() {
-        String text = "Hello, this is an example for you to practice. You should grab " +
-                "this text and make it as your test case." +
+        String text = "Hello, this is an example for you to practice." +
+                " You should grab this text and make it as your test case." +
                 "` ` `javascript" +
                 "if (true) {" +
                 "  console.log('should should should')" +
                 "}" +
                 "` ` `";
-        String text2 = "Hello, this is an example for you to practice. You should grab " +
-                "this text and make it as your test case." +
+
+        String text2 = "Hello, this is an example for you to practice." +
+                " You should grab this text and make it as your test case." +
                 "` ` `Java" +
                 "if (true) {" +
                 "   out.println('should should should')" +
@@ -65,16 +69,22 @@ public class TextProcessingTest {
 
     @Test
     void should_be_ignore_snippets_code_and_analyse() {
-        String text = "Hello, this is an example for you to practice. You should grab " +
-                "this text and make it as your test case." +
+        String text = "Hello, this is an example for you to practice." +
+                " You should grab this text and make it as your test case." +
                 "` ` `Java" +
                 "if (true) {" +
                 "  out.println('should should should')" +
                 "}" +
                 "` ` `";
+
         textProcessor.analyse(text);
         assertThat(textProcessor.getWordsSize()).isEqualTo(21);
         assertThat(textProcessor.getCommonWordsElement(0)).isEqualTo("you");
         assertThat(textProcessor.getCommonWordsElement(1)).isEqualTo("this");
+    }
+
+    @Test
+    void should_be_throw_error_if_pass_null_in_argument() {
+        assertThatThrownBy(()->textProcessor.analyse(null)).hasMessage("text should not be null");
     }
 }
